cc4265ef8ffe3355a4bb60daf7d8c296bf506f5f
Duvion ERP Angola <admin@erp-angola.co.ao>
2026-04-02 01:20:22 +0100

    fix: resolvendo deprecados sugeridos pela Vercel

--- next.config.ts
+++ next.config.ts
@@ -8,11 +8,6 @@ const nextConfig: NextConfig = {
     // Recommended since accurate types require the `supabase gen types` command.
     ignoreBuildErrors: true,
   },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
